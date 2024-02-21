5a8a06d73554d8043e40c7f52447d3f721982c29
rexno7 <rexno7@gmail.com>
2024-02-21 13:41:23 -0600

    fix to template filepath

--- src/dayxx/DayXX.java
+++ src/dayxx/DayXX.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class DayXX {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner reader = new Scanner(new File("src/day16/resources/test.txt"));
+		Scanner reader = new Scanner(new File("src/dayxx/resources/test.txt"));
 		while (reader.hasNextLine()) {
 			String line = reader.nextLine();
 			System.out.println(line);
